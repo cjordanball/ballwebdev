b3147948325246a35b9f1776e7055c7806525f6b
Jordan Ball <cjordanball@ballwebdev.com>
2023-06-01 15:37:28 -0400

    create landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
 import styles from './page.module.css';
+import Link from 'next/link';
+import hatPic from 'public/images/20190201 CJB with hat.jpg';
+import { homePage } from '../content';
 
 const Home = () => {
-	return <div className={styles.container}>What a day!</div>;
+	return (
+		<div className={styles.container}>
+			<div className={styles.titleRegion}>{homePage.headline}</div>
+			<div className={styles.imageRegion}>
+				<img
+					className={styles.theImage}
+					src={'/images/20190201 CJB with hat.jpg'}
+					alt='CJB with hat'
+				/>
+			</div>
+			<div className={styles.welcomeSection}>{homePage.intro}</div>
+			<Link className={styles.buttonStyle} target='_blank' href='/resume'>
+				<button>Go to Résumé</button>
+			</Link>
+		</div>
+	);
 };
 
 export default Home;
