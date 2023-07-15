4cb11f8b18f9c73e70ad23412ff415e8cf81af94
Jordan Ball <cjordanball@ballwebdev.com>
2023-07-14 22:26:14 -0400

    update next version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,22 @@
-import styles from './page.module.css';
+import Image from 'next/image';
 import Link from 'next/link';
-import hatPic from 'public/images/20190201 CJB with hat.jpg';
-import { homePage } from '../content';
+import styles from './page.module.css';
+import { homePage } from '@/content';
 
 const Home = () => {
 	return (
 		<div className={styles.container}>
 			<div className={styles.titleRegion}>{homePage.headline}</div>
 			<div className={styles.imageRegion}>
-				<img
-					className={styles.theImage}
+				<Image
+					className={styles.ImageRegion}
+					width={500}
+					height={300}
 					src={'/images/20190201 CJB with hat.jpg'}
 					alt='CJB with hat'
 				/>
 			</div>
-			<div className={styles.welcomeSection}>{homePage.intro}</div>
-			<Link className={styles.buttonStyle} target='_blank' href='/resume'>
+			<Link className={styles.buttonStyle} target='blank' href='./resume'>
 				<button>Go to Résumé</button>
 			</Link>
 		</div>
